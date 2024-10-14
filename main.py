a1493d1c7a2ec381bd6b50844208124a7de07388
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-10-14 17:31:16 +1000

    done

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from decouple import config
+import requests
 
 TOKEN = config("TOKEN")
-
+API_TOKEN = config('API_TOKEN')
 # All handlers should be attached to the Router (or Dispatcher)
 
 disp = Dispatcher()
@@ -20,6 +21,15 @@ async def command_start_handler(message: Message) -> None:
     '/start' command
     """
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}! Write your city below and I'll send you weather info!")
+
+
+@disp.message()
+async def echo_handler(message: Message) -> None:
+    data = requests.get('https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}'.format(city_name=message.text, API_key=API_TOKEN)).json()
+    try:
+        await message.answer(f'{data['weather'][0]['main']}. {data['weather'][0]['description']}. speed {data['wind']['speed']} m/s')
+    except:
+        await message.answer('Что-то пошло не так. Вводите название города на английском.')
 
 
 async def main() -> None:
